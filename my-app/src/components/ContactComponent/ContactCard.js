3505138a1f0459f197beca8cb4dfcc17a42e9990
Jesse-Tingle <jtingle0@gmail.com>
2019-09-02 20:30:43 -0400

    Changed email and phone icon images, styled ContactCard.js

--- my-app/src/components/ContactComponent/ContactCard.js
+++ my-app/src/components/ContactComponent/ContactCard.js
@@ -1,25 +1,26 @@
 import React from 'react';
 //import phone from '../img/phone.png';
+import {ContainerDiv, ContactH1, ContactSubContainers, Image, SubHeadings} from './contact-styles';
 
 function ContactCard (props) {
     return (
-        <>
-            <h1>Contact</h1>
-            <div>
-                <img src={require('../../img/iPhone.png')} alt="phone icon"/>
-                <h2>+1 (502) 229 - 2383</h2>
-            </div>
+        <ContainerDiv>
+            <ContactH1>Contact</ContactH1>
+            <ContactSubContainers>
+                <Image src={require('../../img/phone.png')} alt="phone icon"/>
+                <SubHeadings>+1 (502) 229 - 2383</SubHeadings>
+            </ContactSubContainers>
 
-            <div>
-                <img src={require('../../img/email.png')} alt="email icon"/>
-                <h2>jtingle0@gmail.com</h2>
-            </div>
+            <ContactSubContainers>
+                <Image src={require('../../img/email.png')} alt="email icon"/>
+                <SubHeadings>jtingle0@gmail.com</SubHeadings>
+            </ContactSubContainers>
 
-            <div>
-                <img src={require('../../img/location.png')} alt="location icon"/>
-                <h2>Kentucky, USA</h2>
-            </div>
-        </>
+            <ContactSubContainers>
+                <Image src={require('../../img/location.png')} alt="location icon"/>
+                <SubHeadings>Kentucky, USA</SubHeadings>
+            </ContactSubContainers>
+        </ContainerDiv>
     )
 }
 
